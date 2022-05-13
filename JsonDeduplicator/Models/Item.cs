e0d5d8c0cf0bd556465408d9f7badbe59b55a9bf
SpaceBeeGaming <space.bee.gaming@gmail.com>
2022-05-13 12:53:20 +0300

    Fixed Equals Method handling of non-same reference.

--- JsonDeduplicator/Models/Item.cs
+++ JsonDeduplicator/Models/Item.cs
@@ -12,21 +12,17 @@ public class Item : IEquatable<Item>
 
     public bool Equals(Item? other) =>
         ReferenceEquals(this, other)
-        && Id == other.Id
-        && Cost == other.Cost
-        && Amount == other.Amount
-        && Date == other.Date
-        && CarKm == other.CarKm;
+        || (Id == other?.Id
+            && Cost == other.Cost
+            && Amount == other.Amount
+            && Date == other.Date
+            && CarKm == other.CarKm);
 
     public override bool Equals(object? obj) => Equals(obj as Item);
     public override int GetHashCode() => HashCode.Combine(Id, Cost, Amount, Date, CarKm);
-
     public static bool Equals(Item? x, Item? y) => x is null ? y is null : x.Equals(y);
-
     public static bool operator ==(Item? item1, Item? item2) => Equals(item1, item2);
-
     public static bool operator !=(Item? item1, Item? item2) => !(item1 == item2);
-
 }
 
 [JsonSerializable(typeof(List<Item>))]
